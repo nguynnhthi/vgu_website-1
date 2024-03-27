4f03e28614fd34673ff31a0b52c09fbb324bef08
Tho_NguyenDuc <ductho892000@gmail.com>
2024-03-27 15:02:10 +0700

    Second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { sans } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${sans.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import { IBM_Plex_Sans, Lusitana } from 'next/font/google';
+
+ 
+export const sans = IBM_Plex_Sans({  weight: ['400'], subsets: ['latin'] });
+export const lusitana = Lusitana({  weight: ['400'], subsets: ['latin'] });
