89ddb197bd6ea53f217dda98d5890c2b5817eac5
Tho_NguyenDuc <ductho892000@gmail.com>
2024-05-21 14:54:00 +0700

    May 21

--- app/ui/organization/nav-links.tsx
+++ app/ui/organization/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import clsx from 'clsx';
 import Link from 'next/link';
@@ -7,46 +7,42 @@ import { usePathname } from 'next/navigation';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-    { name: 'Organzation', href: '/organization', },
-    {
-        name: 'Presidential Boards',
-        href: '/organization/presidentialboard',
+  { name: 'Organzation', href: '/organization' },
+  {
+    name: 'Presidential Boards',
+    href: '/organization/presidentialboard',
+  },
+  {
+    name: 'Falcuties',
+    href: '/organization/falcuties',
+  },
 
-    },
-    {
-        name: 'Falcuties',
-        href: '/organization/falcuties',
-
-    },
-
-    {
-        name: 'Central Units',
-        href: '/organization/centralunits',
-
-    },
+  {
+    name: 'Central Units',
+    href: '/organization/centralunits',
+  },
 ];
 
 export default function NavLinks() {
-    const pathname = usePathname();
-    return (
-        <>
-            {links.map((link) => {
-
-                return (
-                    <Link
-                        key={link.name}
-                        href={link.href}
-                        className={clsx(
-                            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                            {
-                                'bg-sky-100 text-blue-600': pathname === link.href,
-                            },
-                        )}
-                    >
-                        <p className="hidden md:block">{link.name}</p>
-                    </Link>
-                );
-            })}
-        </>
-    );
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'hover:bg-sky-100 flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
+          >
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
 }
